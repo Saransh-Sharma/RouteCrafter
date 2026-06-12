e20f2ec00a8f898a085709c929937d3e0b941e18
Saransh <saransh1337@gmail.com>
2026-06-12 13:39:18 +0530

    Refactor layout components with updated nav and sidebar

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,17 +1,24 @@
 import * as React from "react";
 import { Sidebar } from "./Sidebar";
 import { MobileNav } from "./MobileNav";
+import { PersistenceNotice } from "./PersistenceNotice";
+import { AuthProvider } from "./AuthProvider";
 
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-dvh">
-      <Sidebar />
-      <div className="flex min-w-0 flex-1 flex-col">
-        <MobileNav />
-        <main className="flex-1 px-5 py-8 sm:px-8 lg:px-12 lg:py-12">
-          <div className="mx-auto w-full max-w-6xl">{children}</div>
-        </main>
+    <AuthProvider>
+      <div className="flex min-h-dvh">
+        <Sidebar />
+        <div className="flex min-w-0 flex-1 flex-col">
+          <MobileNav />
+          <main className="flex-1 px-5 py-8 sm:px-8 lg:px-12 lg:py-12">
+            <div className="mx-auto w-full max-w-6xl">
+              <PersistenceNotice />
+              {children}
+            </div>
+          </main>
+        </div>
       </div>
-    </div>
+    </AuthProvider>
   );
 }
