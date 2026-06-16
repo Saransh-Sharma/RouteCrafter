41102b8035e36954eb0fadc5f44f5303df42ab74
Saransh <saransh1337@gmail.com>
2026-06-16 11:26:40 +0530

    Update AppShell layout component

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -3,22 +3,25 @@ import { Sidebar } from "./Sidebar";
 import { MobileNav } from "./MobileNav";
 import { PersistenceNotice } from "./PersistenceNotice";
 import { AuthProvider } from "./AuthProvider";
+import { AiConfigProvider } from "@/components/ai/AiConfigProvider";
 
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
-      <div className="flex min-h-dvh">
-        <Sidebar />
-        <div className="flex min-w-0 flex-1 flex-col">
-          <MobileNav />
-          <main className="flex-1 px-5 py-8 sm:px-8 lg:px-12 lg:py-12">
-            <div className="mx-auto w-full max-w-7xl">
-              <PersistenceNotice />
-              {children}
-            </div>
-          </main>
+      <AiConfigProvider>
+        <div className="flex min-h-dvh">
+          <Sidebar />
+          <div className="flex min-w-0 flex-1 flex-col">
+            <MobileNav />
+            <main className="flex-1 px-5 py-8 sm:px-8 lg:px-12 lg:py-12">
+              <div className="mx-auto w-full max-w-7xl">
+                <PersistenceNotice />
+                {children}
+              </div>
+            </main>
+          </div>
         </div>
-      </div>
+      </AiConfigProvider>
     </AuthProvider>
   );
 }
